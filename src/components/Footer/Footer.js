b09b07ceede4b39bc4fb9a16244f30ddc1bc5bfd
skhan <skhan@staff.compass.edu.au>
2020-11-24 12:47:14 +1100

    footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Footer.css";
+
+
+
+function Footer() {
+
+  return (
+    <footer>
+      <h1>Good Luck With Your Search</h1>
+    </footer>
+  );
+}
+
+export default Footer;
