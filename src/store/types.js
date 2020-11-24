c74c1fcec80949305e5abd9329d5aa98853eaa57
skhan <skhan@staff.compass.edu.au>
2020-11-24 12:50:16 +1100

    Action types

---
+++ src/store/types.js
@@ -0,0 +1,10 @@
+export const GET_PLANETS = "GET_PLANETS";
+export const GET_VEHICLES = "GET_VEHICLES";
+export const FIND_PLANET = "FIND_PLANET";
+export const LOADING = "LOADING";
+export const ERROR = "ERROR";
+
+export const VALID_PLANET_OPTIONS = "VALID_PLANET_OPTIONS";
+export const SELECTED_PLANETS = "SELECTED_PLANETS";
+export const SELECTED_VEHICLES = "SELECTED_VEHICLES";
+export const CLEAR = "CLEAR";
