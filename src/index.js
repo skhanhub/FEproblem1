7b002253b70465742a910ab383d2f5e2ca2b3c45
skhan <skhan@staff.compass.edu.au>
2020-11-24 12:47:02 +1100

    entrypoint

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from "redux"
+import reduxThunk from "redux-thunk"
+import logger from "redux-logger"
+import { Provider } from "react-redux"
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import rootReducer from "./store/reducers";
+
+
+const store = () =>
+  createStore(rootReducer, {}, applyMiddleware(reduxThunk, logger));
+
+ReactDOM.render(
+  <Provider store={store()}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
